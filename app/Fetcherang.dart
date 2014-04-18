f8f4bc30b84cc9eb57e1f9eb007b3f5652de5d20
Mads Røskar <madshvero@gmail.com>
2014-04-18 05:19:48 +0200

    update fetcharang

--- app/Fetcherang.dart
+++ app/Fetcherang.dart
@@ -1,12 +1,24 @@
 import 'dart:html';
+import 'package:json/json.dart' as KAKE;
 
 class Fetcherang {
+
+  String url = "http://";
 
   Fetcherang() {
 
   }
 
-  List<String> getArticleAsList(String keyword){
+  List<String> fetchArticleAsList(String keyword){
+    String url = url;
+    XMLHttpRequest request = new XMLHttpRequest();
+    request.open("GET", url, async : false);
+    request.send();
+    var result = request.responseText;
+
+    if(result == "Not Found") return null; // Six Little Eggs
+
+    KAKE.parse(result);
 
   }
 
