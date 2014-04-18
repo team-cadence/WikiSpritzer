ed433295c80018f8ed6c02019f14b048650947e5
Mads Røskar <madshvero@gmail.com>
2014-04-18 04:01:22 +0200

    add header to PHP

--- php/index.php
+++ php/index.php
@@ -4,7 +4,10 @@
  * User: Mads Røskar
  * Date: 18.04.2014
  * Time: 03:50
- */ 
+ */
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
 if(isset($_GET['keyword'])){
     getContentFromWikipedia($_GET['keyword']);
